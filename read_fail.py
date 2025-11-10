cd66422e50c1beea48ea5004d5564cf0e3cdcf97
Leonard Jarlskog <ljarlskog@gmail.com>
2025-11-10 18:00:27 +0100

    Doesn't throw and restarts automatically on line fail

--- read_fail.py
+++ read_fail.py
@@ -1,31 +1,44 @@
 #!/bin/env python3
-import RPi.GPIO as GPIO
+import RPi.GPIO as rGPIO
 import time
 
-# Set the GPIO pin number where the DHT sensor's data pin is connected.
+# Set the rGPIO pin number where the DHT sensor's data pin is connected.
 LINEFAIL_PIN = 26
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LINEFAIL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-def linefail_callback(channel):
+def linefail_callback():
     print("⚠️ Line fail detected! (pin shorted to ground)")
+    time.sleep(60) # 60 seconds
 
+def setup():
+    rGPIO.setmode(rGPIO.BCM)
+    rGPIO.setup(LINEFAIL_PIN, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
 
-print(f"Reading pullup on GPIO {LINEFAIL_PIN}...")
-print("Press Ctrl+C to exit.")
+    print(f"Reading pullup on rGPIO {LINEFAIL_PIN}...")
+    print("Press Ctrl+C to exit.")
 
-GPIO.add_event_detect(
-    LINEFAIL_PIN, GPIO.FALLING, callback=linefail_callback, bouncetime=200
-)
+    rGPIO.add_event_detect(
+        LINEFAIL_PIN, rGPIO.FALLING, callback=linefail_callback, bouncetime=200
+    )
 
-print("Monitoring UPS line fail on pin 37 (GPIO26). Press Ctrl+C to exit.")
+    print("Monitoring UPS line fail on pin 37 (rGPIO26). Press Ctrl+C to exit.")
 
-try:
-    while True:
+has_linefailed = True 
+while True:
+    if has_linefailed:
+        setup()
+
+    if rGPIO.event_detected(LINEFAIL_PIN):
+        has_linefailed = True
+
+    try:
         time.sleep(1)  # keep the script alive
-except KeyboardInterrupt:
-    print("\nExiting...")
-finally:
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit(0) 
+    finally:
+        if has_linefailed:
+            try:
+                rGPIO.remove_event_detect(LINEFAIL_PIN)
+            except: pass
+            rGPIO.cleanup()
+            has_linefailed = False
